05e5ea642ad68d1a764aa4aa23d9a45b03d698c0
connerc4 <72149361+connerc4@users.noreply.github.com>
2021-04-11 16:31:26 -0400

    Update compile_script.py

--- compile_script.py
+++ compile_script.py
@@ -11,13 +11,9 @@ def run_command(command):
     print('----------------------------------------')
     print('Executing: '+command)
     pipe = subprocess.Popen(command, shell=True, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    #pipe.stdout.close()
     out, err = pipe.communicate()
     print (out)
     
-
-# 0_script 1_list_of_student_usernames 2_directory_to_store_files
-# SAMPLE: compile_script.py level_of_compilation
 print ("Number of arguments: %d" %  len(sys.argv))
 print ("Argument List: %s" % str(sys.argv))
 
@@ -29,7 +25,6 @@ level_of_compilation = int(sys.argv[1])
 
 if level_of_compilation >= 1: 
     for fs in files_to_compile:
-        #cmd = 'avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -I"h:\TEACHING\ECE_387\SPRING_2021\LABS\LAB_4_LCD_LIBRARIES\lcd\lib" -c -o'+fs+'.o '+fs+'.c'
         cmd = 'avr-gcc -Os -DF_CPU=16000000UL -mmcu=atmega328p -c -o'+fs+'.o '+fs+'.c'
         run_command(cmd)
     
